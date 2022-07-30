cb60c66d57f8c1b55476003e14ede26bb81f8859
Suman Jha <100059093+suman-jha-108155@users.noreply.github.com>
2022-07-30 17:58:41 +0530

    Add files via upload

---
+++ Mail Sender/user/Subscribe.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscribe</title>
+    <link rel="stylesheet" href="css/subscribe.css">
+    <link rel="icon" href="https://media1.giphy.com/media/KGI4gmWK9yPsI/giphy.gif" type="image/gif" sizes="16x16">
+    <!--<script type="text/javascript" src="./js/subscribe.js"></script>-->
+</head>
+
+<body>
+    <div id="mainDiv">
+        <form name="subscribe" method="post" action="./php/send_otp.php">
+            <div id="formDiv">
+                <img src="img/email.png" alt="E-mail" id="email-image">
+                <div id="contentDiv">
+                    <h1>XKCD Challange Subscription</h1>
+                    <input type="text" name="email" placeholder="E-mail" id="useremail" />
+                    <ul style="list-style-type: square;"><li id="span-element">OTP will be sent to your Email for Verification.</li>
+                    <ul><br>
+                    <label style="margin-left: 2em;padding-top: 1em;"><b><?php if($_GET)
+                    {echo $_GET['err'];} else{} ?></b></label><br>
+                    <input type="submit" name="Submit" value="Subscribe" /> <br> <br> <br>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <footer>
+        <h4>© Suman Jha</h4>
+    </footer>
+</body>
+
+</html>
---
+++ Mail Sender/user/php/send_otp.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+require_once __DIR__.'/database_connection.php';
+require_once __DIR__.'/config.php';
+
+
+     $db;
+     $query;
+     $user_mail;
+     $otp;
+     $new_otp;
+     $header;
+     $message;
+     $config;
+    
+    function kill()
+    {
+        unset($db);
+        unset($query);
+        unset($user_mail);
+        unset($otp);
+        unset($new_otp);
+        unset($header);
+        unset($message);
+        unset($config);
+    }
+
+    function send_mail_fun($user_mail, $otp){
+        $config = new config();
+        $user_mail = $user_mail;
+        $otp = $otp;
+        $message = '
+            <body style=\'background-color:rgb(238,238,238);padding-top:10px;padding-bottom:10px;text-align:center;\'>
+                <div style=\'width:50%;margin:0 auto;background-color:rgb(248,248,248);padding:10px\'>
+                    <h3>Your OTP for email verification is</h3>
+                    <h1>'.$otp.'</h1>
+                </div>
+            </body>
+        ';
+
+        if(mail($user_mail,'Email Subscription',$message,$config->header)){
+            header("Location: ../Verification.php");
+        }
+        else{
+            echo 'Please try Again';
+        }
+    }
+
+    
+        if(isset($_POST['email'])){
+            $user_mail = $_POST['email'];
+        }
+        else{
+            
+            $err = "Invalid Mail";
+            header("Location: ../Subscribe.php?err=".$err);
+            exit();
+        }
+
+        if (!filter_var($user_mail, FILTER_VALIDATE_EMAIL)) {
+
+            kill();
+            $err = "Invalid Mail";
+            header("Location: ../Subscribe.php?err=".$err);
+            exit();
+        }
+
+        $new_otp = random_int(100000, 999999);
+        $db = new db();
+        $db = $db->database();
+
+        $user_mail = trim($user_mail);
+        $user_mail = htmlspecialchars($user_mail,ENT_QUOTES);
+        $user_mail = mysqli_real_escape_string($db,$user_mail);
+
+        $query = $db->prepare('SELECT * FROM user_data WHERE email=?');
+        $query->bind_param('s',$user_mail);
+        $query->execute();
+        $query->store_result();
+
+        if ($query->num_rows != 0) {
+            kill();
+            $err= "Email is already in use";
+            header("Location: ../Subscribe.php?err=".$err);
+            exit();
+        }
+
+        else{
+            
+
+            send_mail_fun($user_mail,$new_otp);
+            $_SESSION['otp'] = $new_otp;
+            $_SESSION['email'] = $user_mail;
+    }
+
+?>
